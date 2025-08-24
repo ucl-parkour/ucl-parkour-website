410f86063d57d9c08a450dfbac35884abb440580
Mariusz Tang <dev@mariusztang.com>
2025-08-25 00:48:47 +0100

    feat(content): add URLs to the header items

--- src/website.py
+++ src/website.py
@@ -1,19 +1,13 @@
 from staticjinja import Site
 
+from . import context
+
 
 def render(use_reloader=False):
     site = Site.make_site(
         searchpath="src/templates",
         outpath="build",
         staticpaths=["./css/", "./img/", "./fonts/"],
-        env_globals={
-            "header_items": [
-                "Skills",
-                "Spot map",
-                "Gallery",
-                "Committee",
-                "Contact us",
-            ],
-        },
+        env_globals=context.get_global(),
     )
     site.render(use_reloader=use_reloader)
