63f19f89f984de884b7ac98f7b7242cefc102bc3
Mariusz Tang <dev@mariusztang.com>
2025-08-27 17:28:38 +0100

    build: add context class
    This will help standardize future context changes.

--- src/context.py
+++ src/context.py
@@ -2,28 +2,46 @@ import csv
 import os
 from pathlib import Path
 
+
 DATA_DIR = Path(__file__).parent.resolve() / "data"
 
 
 def get_global():
     """Returns the global context which is available in all templates."""
-    global_context = dict()
     dir = DATA_DIR / "global"
-    for path, entry_name in _get_datafiles(dir):
-        with open(path, newline="") as f:
-            entry_data = list(csv.DictReader(f.readlines()))
-            global_context[entry_name] = entry_data
+    global_context = Context("global", dir)
 
-    return global_context
+    for filename in os.listdir(dir):
+        if filename.endswith(".csv"):
+            global_context.add_from_csv(filename)
+
+    return global_context.data
 
 
-def _get_datafiles(dir):
-    """
-    Yields valid data files in dir as tuples containing the full file path
-    and the base file name.
-    """
-    for file in os.listdir(dir):
-        file_name = os.fsdecode(file)
-        entry_name, ext = os.path.splitext(file_name)
-        if ext == ".csv":
-            yield dir / file_name, entry_name
+class Context:
+    def __init__(self, name, source_dir):
+        self.data = dict()
+        self.name = name
+        self.source_dir = source_dir
+
+    def path_from_filename(self, filename):
+        return self.source_dir / filename
+
+    def add_from_csv(self, filename, entryname=None):
+        """
+        Adds the data from the CSV file located in source_dir and assigns it
+        the given entryname.
+
+        If entryname is omitted, uses filename, without the extension.
+        """
+        if entryname is None:
+            basename = os.path.basename(filename)
+            entryname = os.path.splitext(basename)[0]
+
+        if entryname in self.data:
+            print("Warning: Duplicate context data in "
+                  f"{self.name}.{entryname}.")
+
+        with open(self.path_from_filename(filename), newline="") as f:
+            lines = f.readlines()
+        self.data[entryname] = list(csv.DictReader(lines))
