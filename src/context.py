9ffb0c6030784003579edcc9bb55a72ad42531f4
Mariusz Tang <dev@mariusztang.com>
2026-01-01 23:51:16 +0000

    context: reduce Context class to a function
    The only benefit of having the Context class over a simple function is
    that we could add more context files after instantiation.
    
    We don't actually need this ability, so we choose the simpler option.

--- src/context.py
+++ src/context.py
@@ -1,5 +1,4 @@
 import tomllib
-from collections import UserDict
 from pathlib import Path
 
 
@@ -9,7 +8,7 @@ DATA_DIR = Path(__file__).parent.resolve() / "data"
 def get_global(dev_mode):
     """Returns the global context which is available in all templates."""
     dir = DATA_DIR / "global"
-    context = Context("global", dir)
+    context = load_context_data(dir / "global.toml")
 
     header_pages = list()
     for id in context["header_page_ids"]:
@@ -32,11 +31,11 @@ def get_local():
     """Returns the local context which is available in specific templates."""
     dir = DATA_DIR / "local"
 
-    spots = Context("spots", dir).data
+    spots = load_context_data(dir / "spots.toml")
     for spot in spots["spots"]:
         spot["id"] = spot["name"].lower().replace(" ", "-").replace("'", "")
 
-    committee = Context("committee_members", dir).data
+    committee = load_context_data(dir / "committee_members.toml")
 
     return [
         ("spots.html", spots),
@@ -44,15 +43,6 @@ def get_local():
     ]
 
 
-class Context(UserDict):
-    def __init__(self, name, source_dir, *args, **kwargs):
-        self.name = name
-        self.source_dir = source_dir
-        super().__init__(*args, **kwargs)
-        self.add_from_toml(f"{name}.toml")
-
-    def add_from_toml(self, filename):
-        """Adds the data from the TOML file located in source_dir."""
-        with open(self.source_dir / filename, "rb") as f:
-            data = tomllib.load(f)
-        self.data = data
+def load_context_data(path):
+    with open(path, "rb") as f:
+        return tomllib.load(f)
